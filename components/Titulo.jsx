3c78e62ae8afc6a9e65029dc6c5dbc62ba260f2e
ValdirCezar <valdircezar312@gmail.com>
2021-08-16 19:06:33 -0300

    my first components

---
+++ components/Titulo.jsx
@@ -0,0 +1,9 @@
+export default function Titulo(props) {
+git    //console.log(props);
+   return (
+      <>
+         <h1>{props.principal}</h1>
+         <h2>{props.secundario}</h2>
+      </>
+   )
+}
---
+++ pages/basicos/usando_titulo.jsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo";
+
+export default function usandoTitulo() {
+   return (
+      <div>
+         <Titulo 
+            principal="Titulo principal"
+            secundario="Subtitulo"
+         />
+      </div>
+   )
+}
