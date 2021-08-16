a65c159cad65488f43fdf924af90d2a9f2548389
ValdirCezar <valdircezar312@gmail.com>
2021-08-16 17:26:05 -0300

    fragments

---
+++ pages/basicos/fragment2.jsx
@@ -0,0 +1,10 @@
+export default function fragment() {
+   return (
+      <>
+         <h1>Titulo</h1>
+         <h2>subtitulo</h2>
+      </>
+   )
+}
+
+// Componentes funcionais são aqueles componentes gerados a partir de uma função, que é o que vi até aqui
