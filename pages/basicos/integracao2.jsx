e70038abf8a0b98ad33e45f499cb1adc75df827c
ValdirCezar <valdircezar312@gmail.com>
2021-08-17 09:35:40 -0300

    second css file to an component

---
+++ pages/basicos/integracao2.jsx
@@ -0,0 +1,12 @@
+
+import styles from '../../styles/integracao2.module.css'
+
+export default function integracao() {
+   return (
+      <>
+         <div className={styles.vermelha}>Texto#1</div>
+         <div className={styles.azul}>Texto#2</div>
+         <div className={styles.branco}>Texto#3</div>
+      </>
+   )
+}
