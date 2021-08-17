4b56323f005d581f9ed603ac799adac2948a1860
ValdirCezar <valdircezar312@gmail.com>
2021-08-17 09:48:21 -0300

    props read only

---
+++ pages/basicos/somente_leitura.jsx
@@ -0,0 +1,8 @@
+import SomaUm from "../../components/SomaUm";
+import somaUm from "../../components/SomaUm";
+
+export default function somenteLeitura() {
+   return (
+      <SomaUm numero={99} />
+   )
+}
